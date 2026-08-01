7a9cddc8e2e206ec8fc5f0e6d10754b09c5c4510
ruchir_mittal <ruchir.mittal@rapidshyp.com>
2026-08-01 20:10:42 +0530

    1 aug Commit

---
+++ duck_typing.py
@@ -0,0 +1,22 @@
+# Duck Typing = Another way to achieve polymorphism besides Inheritance
+#               Object must have the minimum necessary attributes/methods
+
+class Animal:
+    alive = True
+
+class Dog(Animal):
+    def speak(self):
+        print("WOOF")
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW")
+
+class Car: 
+    def speak(self):
+        print("HONK")
+
+animals = [Dog(), Cat(), Car()]
+
+for animal in animals: 
+    animal.speak()
