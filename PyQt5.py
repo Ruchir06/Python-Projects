ff72b6f1bdc09db4fbe458dfc6b233fe032e30b7
ruchir_mittal <ruchir.mittal@rapidshyp.com>
2026-08-11 00:42:07 +0530

    Final changes

---
+++ PyQt5.py
@@ -0,0 +1,14 @@
+# PyQt5 Introduction 
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
--- api.py
+++ api.py
@@ -1,16 +1,19 @@
 import requests
-
 base_url = "https://pokeapi.co/api/v2/"
 
 def get_pokemon_info(name):
-    url= f"{base_url}/pokemon/{name}"
+    url = f"{base_url}/pokemon/{name}"
     response = requests.get(url)
-    if response.status_code == 200:
+    if response.status_code == 200: 
         pokemon_data = response.json()
-        print(pokemon_data)
-    else:
+        return pokemon_data
+    else: 
         print(f"failed to retrieve data {response.status_code}")
 
-pokmeon_name = "pikachu"
+pokemon_name = "squirtle"
+# pokemon_name = "pikachu"
+pokemon_info = get_pokemon_info(pokemon_name)
 
-get_pokemon_info(pokmeon_name)
+if pokemon_info:
+    print(f"{pokemon_info["name"]}")
+    print(f"{pokemon_info["id"]}")
