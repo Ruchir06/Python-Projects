3ad86276bc0f456127c4c4b635a95042b14ed450
ruchir_mittal <ruchir.mittal@rapidshyp.com>
2026-08-17 01:51:37 +0530

    Django First Commit

---
+++ cricket_club/members/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    age = models.IntegerField(default=30)
+    DOB = models.IntegerField()
+    joined_date = models.DateField()
+
+    
+
+
+class Kit(models.Model):
+    name = models.CharField(max_length=255)
